d50f71ae18356a1ef558d45bc4a3db7261864bc0
TakeokaShogo <95583179+TakeokaShogo@users.noreply.github.com>
2022-06-06 23:47:31 +0900

    first commit

---
+++ jikken2-wordpress.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+sudo apt install -y ibus-mozc
+#reboot
+#migiue 'A' icon
+sudo raspi-config
+#Interface Options -> ssh enable
+#sudo lsof -i:22		#kakuninn
+sudo apt install -y apache2 default-mysql-server php php-mysql
+#sudo systemctl enable apache2.service
+#sudo systemctl status apache2.service	#kakuninn
+mysql -u root
+#mysql> create database wordpress default character set utf8;
+#mysql> grant all on wordpress(db name).* to wordpress@localhost identified by 'password';
+#このマシンでは間違えてデータベースのパスワードが'pasword'になっている（Sが一個少ない）
+#mysql> flush privileges;	(kenngenn hennkou yuukouka)
+#sudo systemctl enable mysqld.service
+#sudo systemctl status mysqld.service	#kakuninn
+cd /var/www/html 	#Apache2 document root
+sudo wget https://ja.wordpress.org/latest-ja.tar.gz
+sudo unar latest-ja.tar.gz
+sudo chown -R www-data:www-data .
+ls -l	#kakuninn
